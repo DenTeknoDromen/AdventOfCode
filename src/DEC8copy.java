e9dd6cc1b69306c2f4a265ff0ef117a08a69e146
Tobias Jansson <tobias.janssn@gmail.com>
2023-12-09 13:16:46 +0100

    DEC7 igen

--- src/DEC8copy.java
+++ src/DEC8copy.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 import java.util.HashMap;
 import java.util.ArrayList;
 
-public class DEC8 {
+public class DEC8copy {
     static HashMap<String, String[]> mapHashMap = new HashMap<String, String[]>();
     static ArrayList<String> stringend = new ArrayList<String>();
-    //static int check = 0;
 
     public static void main(String[] args) throws Exception{
-        File input = new File("DEC8_input.java");
+        File input = new File("DEC8_input.txt");
         Scanner reader = new Scanner(input);
         
         char[] directions = reader.nextLine().toCharArray();
@@ -36,32 +35,16 @@ public class DEC8 {
     static int navigate(char[] directions) {
         int steps = 0;
         String curr_node = "AAA";
-        int check = 0;
+
         int i = 0;
-        //while (!curr_node.equals("ZZZ") && steps < 100000) {        
-        while (check != stringend.size() ) {
-/*          curr_node = (directions[i] == 'L') ? mapHashMap.get(curr_node)[0] : mapHashMap.get(curr_node)[1];
-            System.out.println(curr_node); */
-            check = 0;
-            for (int a = 0; a < stringend.size(); a++) {
-                curr_node = stringend.get(a);
-                curr_node = (directions[i] == 'L') ? mapHashMap.get(curr_node)[0] : mapHashMap.get(curr_node)[1];
-                stringend.set(a, curr_node);
-                
-                //System.out.println(stringend.get(a));
-                check = (curr_node.charAt(2) == 'Z') ? check + 1 : check + 0;
-                ///System.out.println("check: " + check);                
-            }
-            //System.out.println(check);
-            //System.out.println("--------------------------------------------------------------");
+        while (curr_node.toString() != "ZZZ".toString() && steps < 100000) {        
+            curr_node = (directions[i] == 'L') ? mapHashMap.get(curr_node)[0] : mapHashMap.get(curr_node)[1];
+            System.out.println(curr_node);
             i += 1;
             if (i == directions.length) {
                 i = 0;
             }
             steps += 1;
-            if (steps % 100 == 0) {
-                System.out.println("!");
-            }
         }
         return steps;
     }
